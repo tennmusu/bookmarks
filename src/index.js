613c5259674feec296128fe03947498cae52ff54
imaiyukari <imai.yukari.23@gmail.com>
2022-01-03 21:48:46 +0900

    削除機能実装

--- src/index.js
+++ src/index.js
@@ -2,15 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import $ from 'jquery'; 
-function Card (props) {
-  
-  return (
-    <button 
-    className="card" 
-    >
-      <a href={props.value[1]} target="_blank" rel="noopener noreferrer"> {props.value[0]}</a>
-    </button>
-  );
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num:this.props.num,
+      delete:this.props.delete
+    };
+  }
+  render(){
+    return (
+      <div className="cardclass">
+      <button 
+      className="card" 
+      >
+        <a href={this.props.value[1]} target="_blank" rel="noopener noreferrer"> {this.props.value[0]}</a>
+      </button>
+       <button 
+       className="delete"  onClick={()=>this.state.delete(this.state.num)}
+       >
+         __
+       </button>
+       </div>
+    );
+
+  }
+ 
 
 }
 
@@ -18,15 +35,17 @@ class List extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Cards:props.Cards,
-      title:"",
+      Cards:this.props.Cards,
+      num:this.props.num,
+      store:this.props.store
     };
   }
 
 renderCard(i) {
  
   return (<Card value={this.state.Cards[i]}
-    //onClick={() => this.props.onClick(i)}
+        delete={this.delete.bind(this)}
+        num={i}
   />);
   
 }
@@ -38,24 +57,35 @@ const chars = url.split(' ');
 Cards.push(chars);
   this.setState({Cards:Cards ,
   });
+  this.state.store(this.state.num,Cards);
  }
+ delete(i) {
+  const Cards=this.state.Cards.slice();
+  Cards.splice(i, 1);
+    this.setState({Cards:Cards ,
+    });
+    this.state.store(this.state.num,Cards);
+   }
+ 
 render() {
   return (
-    <div>
-    {this.state.Cards.map((val, i) => {
-              return (
-               this.renderCard(i)
-              );
-            
-          
-          })}   
-    <button 
-    className="AddCard" 
-    onClick={() => this.AddClick()}
-    >
-      {"+"} 
-    </button>
-</div>
+        <div>
+        {this.state.Cards.map((val, i) => {
+                  return (
+                   this.renderCard(i)
+                  );
+                
+              
+              })}   
+        <button 
+        className="AddCard" 
+        onClick={() => this.AddClick()}
+        >
+          {"+"} 
+        </button>
+     </div>
+       
+   
   );
 }
 }
@@ -64,38 +94,88 @@ class Home extends React.Component {
 constructor(props) {
   super(props);
   this.state = {
-    Lists:Array(1).fill(Array(0).fill(null)),
-  };
+    Lists:Array(0).fill(null),
+    names:Array(0).fill(null),
+    isHome:true,
+    current:null,
+    num:null,
+  }
+  this.callback = this.store.bind(this)
+  
 }
 AddClick() {
   const Lists=this.state.Lists.slice();
+  const names=this.state.names.slice();
+  var name=prompt("カテゴリーの名前を入力してください");
+  names.push(name);
   Lists.push(Array(0).fill(null));
     this.setState(
-      {Lists:Lists},
+      {Lists:Lists,
+       names:names
+      },
       )
   }
+  nameClick(i) {
+    this.setState(
+       {isHome:!this.state.isHome,
+        current:this.state.Lists[i],
+        num:i
+      }
+      )
+    }
+    store(i,v) {
+      const Lists=this.state.Lists.slice();
+      Lists[i]=v;
+      this.setState(
+        {Lists:Lists
+        }
+        )
+      }
 render() {
-  const Lists = this.state.Lists;
-  const datas = Lists.map((step, i) => {
+  const names = this.state.names;
+  const data =
+(<div className="list">
+    <List 
+   Cards={this.state.current}
+   num={this.state.num}
+   store={this.callback}
+   />
+ </div> );
+  const category = names.map((step, i) => {
    
     return (
-      <div className="list">
-      <List 
-     Cards={this.state.Lists[i]}
-     />
-   </div> 
+     <button className="category" 
+     onClick={() => this.nameClick(i)}
+     >
+      {this.state.names[i]}
+     </button>
     );
   });
- 
+
+  const back =(
+    <button className="back" 
+    onClick={ ()=> this.setState(
+      {isHome:!this.state.isHome
+      }
+      )
+    }
+    >
+     {"ホームに戻る"}
+    </button>
+   );
   return (
     <div className="home">
-       {datas}
-    <button 
+       {this.state.isHome===true?category:back}
+       {this.state.isHome===false?data:null}
+   {this.state.isHome===true?
+   <button 
     className="AddList" 
     onClick={() => this.AddClick()}
     >
       {"+"} 
-    </button>
+      </button>
+      :null  
+      }
     </div>
   );
 }
@@ -104,6 +184,7 @@ render() {
 // ========================================
 
 ReactDOM.render(
-<Home />,
+<Home
+/>,
 document.getElementById('root')
 );
